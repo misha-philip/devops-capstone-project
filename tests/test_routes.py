60e8d9b96621b2e5877a50b2f9e817a03ebd00f3
misha-philip <michaelpgilippyev@gmail.com>
2024-12-11 16:00:49 -0500

    updated files in pipeline and test_routes

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -14,12 +14,12 @@ DATABASE_URI = os.getenv(
 BASE_URL = "/accounts"
 HTTPS_ENVIRON = {'wsgi.url_scheme': 'https'}
 
+
 ######################################################################
 #  T E S T   C A S E S
 ######################################################################
 class TestAccountService(TestCase):
     """Account Service Tests"""
-
 
     @classmethod
     def setUpClass(cls):
@@ -30,7 +30,6 @@ class TestAccountService(TestCase):
         app.logger.setLevel(logging.CRITICAL)
         init_db(app)
         talisman.force_https = False
-
 
     @classmethod
     def tearDownClass(cls):
